ffb3c148cd055d344d239aad4e79e550b130ca26
Gustavo Adolfo Minotas Ruiz <guscolombiamex@gmail.com>
2020-11-04 10:54:13 -0500

    crear funciones

--- Ejercicio3/Ejercicio3.js
+++ Ejercicio3/Ejercicio3.js
@@ -7,12 +7,19 @@ let sueldo;
 let dinero;
 let ano;
 
-dinero = Number(prompt ("Ingrese cuanto ahorra" ));
+dinero = Number(prompt ("Ingrese cuanto es el sueldo" ));
 
-semana = dinero*0.15;
+semana = Semana (dinero);
 mes = 4* semana;
 ano = 12*mes;
 
 alert('total ahorrado:' + ano);
 alert('cada semana ahorra 15%' + semana);
 document.write ("total ahorrado:" + ano)
+
+//funciones
+function Semana (dinero){
+    semana = dinero*0.15;
+    return semana
+}
+
